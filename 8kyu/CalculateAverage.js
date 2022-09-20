f8a09509932a29f4b4978cbeaf7783d0c7295525
Tatyana Nemchina <23angorka07@gmail.com>
2022-09-20 14:13:45 +0300

    second solution to the task CalculateAverage

--- 8kyu/CalculateAverage.js
+++ 8kyu/CalculateAverage.js
@@ -1,5 +1,6 @@
 // The task is to calculate the average of the numbers in a given list.
 
+// My first solution
 function findAverage(array) {
   if (array.length !== 0) {
     let total = 0;
@@ -9,4 +10,9 @@ function findAverage(array) {
     return total / array.length;
   }
   return 0;
+}
+
+//After learning about the metod REDUCE - my second solution
+function findAverage(array) {
+  return array.length === 0 ? 0 : array.reduce((prev, item) => prev + item) / array.length;
 }
